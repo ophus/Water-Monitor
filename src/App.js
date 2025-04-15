dfbbef59bb96b85d47fa3521573796c0db54ee2d
ophus <phucminh9395@gmail.com>
2025-04-15 16:53:31 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import WaterMonitorWebsite from './WaterMonitorWebsite.jsx';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <WaterMonitorWebsite />
+    </div>
+  );
+}
+
+export default App;
